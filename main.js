4882b0265d48211fa0448748ab332643f73e0c2f
karyasuta <81769360+karyasuta@users.noreply.github.com>
2022-12-03 22:30:18 +0800

    Add playRound logic function

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ function getPlayerChoice(pick) {
     return choice;
   }
 }
+
+function playRound() {
+  let computerSelection = getComputerChoice();
+  let playerSelection = getPlayerChoice();
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    return `    Player: ${playerSelection} 
+    Computer: ${computerSelection}.
+    Congrats, PLAYER! You WON this round.`;
+  } else if (playerSelection === computerSelection) {
+    return `    Player: ${playerSelection} 
+    Computer: ${computerSelection}.
+    DRAW!`;
+  } else {
+    return `    Player: ${playerSelection} 
+    Computer: ${computerSelection}.
+    Congrats, COMPUTER! You WON this round.`;
+  }
+}
